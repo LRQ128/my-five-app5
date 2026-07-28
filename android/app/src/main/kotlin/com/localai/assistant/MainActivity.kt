7355771298ba33294fee9b14a61d338aa7a9b948
li-ruiquan-123 <li-ruiquan@163.com>
2026-07-28 08:42:59 +0800

    fix: 修复MainActivity.kt中FlutterActivity导入typo + 添加缺失的styles.xml

--- android/app/src/main/kotlin/com/localai/assistant/MainActivity.kt
+++ android/app/src/main/kotlin/com/localai/assistant/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.localai.assistant
 
-import io.flutter.embedding.android.Flutter冇有Activity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
+class MainActivity: FlutterActivity() {
 }
